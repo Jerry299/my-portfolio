94ee0253170ccbce6eecab5b6ac76892b9b2ff87
Jerry299 <josephjeremiah564@yahoo.com>
2020-09-22 04:48:09 +0100

    scaled svg for contact page

--- src/components/statefulComponents/ContactMe.js
+++ src/components/statefulComponents/ContactMe.js
@@ -1,14 +1,29 @@
 import React from "react";
 import contactSVG from "../../images/contactSVG.svg";
+import "./ContactMe.css";
 
 const ContactMe = () => {
   return (
     <div className="contact-container">
       <div className="contact-wrapper">
-        <div className="contact-form"></div>
         <div className="contact-svg">
           <img src={contactSVG} alt="Contact-svg" />
         </div>
+        <div className="contact-msg">
+          <p>
+            like what you see? have a project that you'd like to talk
+            about,looking for a dev to hire? or want to ask a personal question?
+          </p>
+          <div className="get-in-touch">
+            <h1>
+              <span>Get in </span>
+              <p>touch</p>
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div className="contact-form">
+        <input></input>
       </div>
     </div>
   );
