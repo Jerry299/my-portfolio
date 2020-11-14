4cfa4dc30b3e89c95cec9cbee1180a89a558a358
Jerry299 <josephjeremiah564@yahoo.com>
2020-11-14 06:34:02 +0100

    adding tiles for projects

--- src/components/statelessComponents/Work.js
+++ src/components/statelessComponents/Work.js
@@ -1,37 +1,48 @@
-import React from 'react';
+ import React from 'react';
 import "./MyWorks.scss";
 
 const Work = (props) => {
     return (
-        
-        <div className="project">
-            <div className="project-details">
+      
+          <div classNameName="project">
+            <div classNameName="project-details">
               <p id="project-name">{props.name}</p>
-              <div className="project-intro"> {props.projectIntro} </div>
+              <div classNameName="project-intro"> {props.projectIntro} </div>
             </div>
 
-            <div className="card">
-              <div className="card-front" style={{backgroundImage:`url(${props.bgImage})`}} ></div>
-              <div className="card-back">
-                {/* <h2>
-                  {props.backHeader}
-                  <br />
-                  <span> {props.backHeaderSpan} </span>
-                </h2> */}
-                <div className="projects-links">
-                  <div className="butn_cont">
+            <div classNameName="card">
+              <div classNameName="card-front" style={{backgroundImage:`url(${props.bgImage})`}} ></div>
+              <div classNameName="card-back">
+               
+                <div classNameName="projects-links">
+                  <div classNameName="butn_cont">
                     <a href={`${props.githubLink}`} target="_blank" rel="noreferrer noopener">Link To Github</a>
                   </div>
-                  <div className="butn_cont">
+                  <div classNameName="butn_cont">
                     <a href={`${props.appLink}`} target="_blank" rel="noreferrer noopener">Link To App</a>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-
-    
     );
 };
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 export default Work;
