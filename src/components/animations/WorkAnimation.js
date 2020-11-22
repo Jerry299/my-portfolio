feb1ec70f1640b5e7e49bc6def3ebd21a1264f35
Jerry299 <josephjeremiah564@yahoo.com>
2020-11-22 19:33:26 +0100

    added responsiveness for work component

--- src/components/animations/WorkAnimation.js
+++ src/components/animations/WorkAnimation.js
@@ -1,3 +1,20 @@
 import {gsap} from 'gsap';
 
 
+ const openUp = (el) => {
+    console.log("openup")
+    gsap.to(el,{
+        duration:1,
+        ease: "elastic.out(1.75, 0.3)",
+        css: {display: "block"}
+    })
+}
+
+ const closeUp = (el) => {
+    console.log("closeup")
+    gsap.to(el,{
+        duration:1,
+        ease: "elastic.out(1.75, 0.3)",
+        css: {display: "none"}
+    })
+}
--- src/components/statelessComponents/Work.js
+++ src/components/statelessComponents/Work.js
@@ -1,25 +1,20 @@
- import React,{useEffect,useRef} from 'react';
+import React,{useEffect,useRef} from 'react';
 import "./MyWorks.scss";
+
+
 
 const Work = (props) => {
   //variables for selecting DOM nodes
   let wrapper = useRef(null)
-  const enterMouse = () => {
-    console.log("Mouse don Enter")
-  }
-  useEffect(() => {
-    
-  })
-
+  
+ 
 
   return (
     <div className="work-container">
       
       {/* project start */}
-      <div className={`work-wrapper ${props.id % 2 === 0 ? 'work-wrapper-even' : 'work-wrapper-odd'}`}
-        ref={el => (wrapper = el)}
-      >
-        <div className="card-info" onMouseEnter={enterMouse}>
+      <div className={`work-wrapper ${props.id % 2 === 0 ? 'work-wrapper-even' : 'work-wrapper-odd'}`}>
+        <div className="card-info" >
           <div>
             <h2>{props.name}</h2>
             <p className="intro"> {props.intro} </p>
@@ -32,7 +27,7 @@ const Work = (props) => {
               <img src={`${props.imageUrl}`} alt="" className="img"/>
             </div>
 
-          <div className="body_post">
+          <div className="body_post"  ref={el => (wrapper = el)}>
             <div className="body_post_info">
               <div className="body_post_links">
                 <div className="butn-cover">
