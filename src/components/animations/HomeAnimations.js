e7a24ffa10dfe91bf813798cc782175fa14f1648
Joseph Jerry <josephjeremiah564@yahoo.com>
2021-01-07 05:36:59 +0100

    adjusted the work component

--- src/components/animations/HomeAnimations.js
+++ src/components/animations/HomeAnimations.js
@@ -1,97 +1,96 @@
-import {gsap} from 'gsap'
+import { gsap } from "gsap";
 
 import { TextPlugin } from "gsap/TextPlugin";
 
 gsap.registerPlugin(TextPlugin);
-let words = ["Full Stack Javascript Developer...","Passionate Learner...","Technology Enthusiast..."];
+let words = [
+  "Full Stack Javascript Developer...",
+  "Passionate Learner...",
+  "Technology Enthusiast...",
+];
 
-let masterTl = gsap.timeline({repeat: -1}).pause();
+let masterTl = gsap.timeline({ repeat: -1 }).pause();
 
 let homeTl = gsap.timeline();
-let boxTl = gsap.timeline()
+let boxTl = gsap.timeline();
 
+export const animationForHomeHiandHomeName = (el1, el2, el3, el4, el5) => {
+  homeTl.from(el1, {
+    duration: 2.3,
+    y: -300,
+    repeat: 0,
+    ease: "elastic.out(1, 0.3)",
+  });
+  homeTl.from(el2, {
+    duration: 1.9,
+    x: -500,
+    opacity: 0,
+    ease: "elastic.out(2, 0.3)",
+  });
+  homeTl.from(el3, {
+    duration: 1,
+    y: 200,
+    opacity: 0,
+    ease: "bounce.out",
+  });
+  homeTl.from(el4, {
+    duration: 1,
+    y: -200,
+    opacity: 0,
+    ease: "elastic.out(1.75, 0.3)",
+  });
+  homeTl.from(el5, {
+    duration: 1,
+    x: 200,
+    opacity: 0,
+    ease: "elastic.out(1.75, 0.3)",
+  });
+};
 
-
-export const animationForHomeHiandHomeName = (el1,el2,el3,el4,el5) => {
-    
-    homeTl.from(el1,{
-        duration: 2.3,
-        y:-300,
-        repeat: 0,
-        ease: "elastic.out(1, 0.3)"
+export const underlineTextAndFadeUp = (el, el2) => {
+  boxTl
+    .to(el, {
+      duration: 4,
+      width: "77px",
+      ease: "power4.inOut",
     })
-    homeTl.from(el2,{
-        duration: 1.9,
-        x:-500,
-        opacity: 0,
-        ease: "elastic.out(2, 0.3)",
+    .from(el2, {
+      duration: 4,
+      y: 50,
+      opacity: 0,
+      ease: "power3.out",
+      onComplete: () => masterTl.play(),
     })
-    homeTl.from(el3,{
-        duration:1,
-        y:200,
-        opacity:0,
-        ease: "bounce.out",
+    .to(el, {
+      duration: 4,
+      height: "34px",
+      ease: "elastic.out",
     })
-     homeTl.from(el4,{
-        duration:1,
-        y:-200,
-        opacity:0,
-        ease: "elastic.out(1.75, 0.3)",
-    })
-     homeTl.from(el5,{
-        duration:1,
-        x:200,
-        opacity:0,
-        ease: "elastic.out(1.75, 0.3)",
-    })
-}
-
-export const underlineTextAndFadeUp = (el,el2) => {
-    boxTl.to(el,{
-        duration: 4,
-        width: "77px",
-        ease: "power4.inOut"
-    })
-    .from(el2,{
-        duration: 4.5,
-        y : 50,
-        opacity:0,
-        ease: "power3.out",
-        onComplete: () => masterTl.play()
-    })
-    .to(el,{
-        duration: 4,
-        height: "34px",
-        ease: "elastic.out"
-    })
-    .to(el,{
-        duration: 4.5,
-        autoAlpha:0.5,
-        yoyo: true,
-        repeat: -1,
-        ease: "power4.inOut"
-    })
-}
+    .to(el, {
+      duration: 4.5,
+      autoAlpha: 0.5,
+      yoyo: true,
+      repeat: -1,
+      ease: "power4.inOut",
+    });
+};
 
 export const homeAnimationsForMySkills = (element) => {
-    gsap.to(element,{
-   opacity:0,
-   ease: "power2.inOut",
-   repeat: -1
- })
-}
+  gsap.to(element, {
+    opacity: 0,
+    ease: "power2.inOut",
+    repeat: -1,
+  });
+};
 
 export const wordsAnimation = (textElement) => {
-    words.forEach(word => {
-        let tl = gsap.timeline({repeat: 1,yoyo: true,repeatDelay: 1.5});
-        tl.to(textElement,{
-            duration: 1.9,
-            text: word,
-        })
-       
-        masterTl.add(tl)
-    })
-} 
+  words.forEach((word) => {
+    let tl = gsap.timeline({ repeat: 1, yoyo: true, repeatDelay: 1.5 });
+    tl.to(textElement, {
+      duration: 1.9,
+      text: word,
+    });
 
-
-
+    masterTl.add(tl);
+  });
+};
--- src/components/statelessComponents/Work.js
+++ src/components/statelessComponents/Work.js
@@ -1,50 +1,60 @@
-import React,{useEffect,useRef} from 'react';
+import React, { useEffect, useRef } from "react";
 import "./MyWorks.scss";
-
-
 
 const Work = (props) => {
   //variables for selecting DOM nodes
-  let wrapper = useRef(null)
-  
- 
+  let wrapper = useRef(null);
 
   return (
     <div className="work-container">
-      
       {/* project start */}
-      <div className={`work-wrapper ${props.id % 2 === 0 ? 'work-wrapper-even' : 'work-wrapper-odd'}`}>
-        <div className="card-info" >
+      {/* this div immediately below just reverses the layout of the card if it is odd  */}
+      <div
+        className={`work-wrapper ${
+          props.id % 2 === 0 ? "work-wrapper-even" : "work-wrapper-odd"
+        }`}
+      >
+        <div className="card-info">
           <div>
-            <h2>{props.name}</h2>
-            <p className="intro"> {props.intro} </p>
-            <p className="tags"><span className="solid_tag">Tags: </span> <span className="tag_items">{props.tag}</span></p>
+            <h2>{props.name}.</h2>
+            <p className="intro"> {props.intro}. </p>
+            <p className="tags">
+              <span className="solid_tag">Tags: </span>{" "}
+              <span className="tag_items">{props.tag}.</span>
+            </p>
           </div>
         </div>
-        
-        <div className="cards">
-            <div className="header-image">
-              <img src={`${props.imageUrl}`} alt="" className="img"/>
-            </div>
 
-          <div className="body_post"  ref={el => (wrapper = el)}>
-            <div className="body_post_info">
-              <div className="body_post_links">
-                <div className="butn-cover">
-                  <a href="" target="_blank" rel="noreferrer noopener" className="butn">See On Github</a>
-                  <a href="" target="_blank" rel="noreferrer noopener" className="butn2">See Live</a>
-                </div> 
-              </div>
+        <div className="cards">
+          <div className="header-image">
+            <img src={`${props.imageUrl}`} alt="" className="img" />
+          </div>
+
+          <div className="body_post" ref={(el) => (wrapper = el)}>
+            <div className="butn-cover">
+              <a
+                href=""
+                target="_blank"
+                rel="noreferrer noopener"
+                className="butn"
+              >
+                See On Github
+              </a>
+              <a
+                href=""
+                target="_blank"
+                rel="noreferrer noopener"
+                className="butn2"
+              >
+                See Live
+              </a>
             </div>
           </div>
         </div>
-            
-      </div> 
+      </div>
       {/* project ends */}
-      
     </div>
   );
 };
-
 
 export default Work;
