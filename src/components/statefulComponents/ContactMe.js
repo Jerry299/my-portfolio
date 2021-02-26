6fa741198b8fd686b140d22ce6381e77150f4b49
Joseph Jerry <josephjeremiah564@yahoo.com>
2021-02-26 05:39:46 +0100

    made responsive

--- src/components/statefulComponents/ContactMe.js
+++ src/components/statefulComponents/ContactMe.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import contactSVG from "../../images/contactSVG.svg";
 import "./ContactMe.css";
 import Footer from "../statelessComponents/Footer";
@@ -7,6 +7,16 @@ import { useIntersection } from "react-use";
 import { fadeIn, fadeOut, svgAnimate } from "../animations/ContactmeAnimations";
 
 const ContactMe = () => {
+  // state for input
+
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    contactmessage: "",
+  });
+  const [errors, setErrors] = useState("");
+  console.log(errors);
+
   //variables for Dom API
   let formRef = useRef(null);
   let svgRef = useRef(null);
@@ -16,7 +26,7 @@ const ContactMe = () => {
   useEffect(() => {
     document.title = "Contact Jerry";
     svgAnimate(svgRef, contactMessage, getInTouch);
-  }, []);
+  }, [svgRef]);
   const intersection = useIntersection(formRef, {
     root: null,
     rootMargin: "0px",
@@ -26,6 +36,54 @@ const ContactMe = () => {
   intersection && intersection.intersectionRatio < 0.8
     ? fadeOut(".contact-form")
     : fadeIn(".contact-form");
+
+  //event handlers
+
+  const handleChange = (e) => {
+    let { name, value } = e.target;
+    setState((prevState) => ({ ...prevState, [name]: value }));
+  };
+  // form validation
+  const validateForm = () => {
+    let formIsValid = true;
+    let errors = {};
+
+    const { name, email, contactmessage } = state;
+    //regex for name validation
+    let regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
+    let emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (!name || !regName.test(name)) {
+      formIsValid = false;
+      errors["nameError"] = "Invalid Full Name";
+    }
+    if (!email || !emailPattern.test(email)) {
+      formIsValid = false;
+      errors["emailError"] = "Please Enter Your Email";
+    }
+    if (!emailPattern.test(email)) {
+      formIsValid = false;
+      errors["emailError"] = "Invalid Email";
+    }
+    if (!contactmessage) {
+      formIsValid = false;
+      errors["contactMessageError"] = "Please Leave A Message";
+    }
+    if (contactmessage.length < 4) {
+      formIsValid = false;
+      errors["contactMessageError"] =
+        "Message shouldn't be less than 4 letters";
+    }
+    if (contactmessage.length > 35) {
+      formIsValid = false;
+      errors["contactMessageError"] =
+        "Message shouldn't be more than 35 letters";
+    }
+
+    setErrors(errors);
+
+    return formIsValid;
+  };
+
   return (
     <div className="contact-container">
       <div className="contact-wrapper">
@@ -49,31 +107,44 @@ const ContactMe = () => {
         <form className="contact-form">
           <div className="form-wrapper">
             <div className="form-group">
-              <input type="text" name="name" required autoComplete="off" />
+              <input type="text" name="name" required onChange={handleChange} />
               <label className="label-name">
-                <span className="content-name">Name</span>
+                {" "}
+                <span className="content-name">Full Name</span>
               </label>
             </div>
+            {errors.nameError ? <small>{errors.nameError}</small> : null}
             <div className="form-group">
-              <input type="email" name="email" required autoComplete="off" />
+              <input
+                type="email"
+                name="email"
+                required
+                autoComplete="off"
+                onChange={handleChange}
+              />
               <label className="label-name">
                 {" "}
                 <span className="content-name">Email</span>
               </label>
-              <small></small>
             </div>
+            {errors.emailError ? <small>{errors.emailError}</small> : null}
             <div className="form-group">
               <input
                 type="text"
-                name="contact-message"
+                name="contactmessage"
                 required
                 autoComplete="off"
+                onChange={handleChange}
               />
               <label className="label-name">
+                {" "}
                 <span className="content-name">Message</span>
               </label>
             </div>
-            <div type="submit" className="submit">
+            {errors.contactMessageError ? (
+              <small>{errors.contactMessageError}</small>
+            ) : null}
+            <div type="submit" className="submit" onClick={validateForm}>
               Send it
               <div className="line"></div>
             </div>
