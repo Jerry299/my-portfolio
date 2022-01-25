09d1ae9dc519e29e782069a4e7eabae701f3efa0
Jerry299 <josephjeremiah564@yahoo.com>
2022-01-25 02:30:20 +0100

    redesign ongoing

--- src/components/newComponents/statelessComponents/Home/Home.js
+++ src/components/newComponents/statelessComponents/Home/Home.js
@@ -3,7 +3,25 @@ import './Home.css'
 
 const Home = () => {
     return (
-        <div className='home-container'>Hello World</div>
+        <div className='home-container'>
+            <div className='home-header'>
+                <div className='home-logo'>jeremiah</div>
+                <div>hamburger</div>
+            </div>
+            <div className='home-row'>
+                <div className='home-row_A'>
+                    <div  className='home_wrapper'>
+                        <p>Hi there <span></span> I'm</p>
+                        <p>Jeremiah</p>
+                        <p><span>Javascript Developer</span></p>
+                        <p><span>lorem ipsum</span></p>
+                    </div>
+                </div>
+                <div className='home-row_B'>
+                    Home row B
+                </div>
+            </div>
+        </div>
     )
 }
 
